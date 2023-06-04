787386849fec5a695d66bc55677a83fe18fe9172
Aldrin Saurov Sarker <a.saurov2016@gmail.com>
2023-06-04 23:35:50 +0600

    Summarized latest audit

--- audit.php
+++ audit.php
@@ -7,16 +7,20 @@
 
     if ($_GET['action'] == 1) {
         $log = 'Student id '.$_GET['id'].' gave attendance.';
+        $summary = 'Verified';
     } elseif ($_GET['action'] == 2) {
         $log = 'A new student with id '.$_GET['id'].' registered.';
+        $summary = 'Enrolled';
     } elseif ($_GET['action'] == 3) {
         $log = 'Student failed to register as a student with id '.$_GET['id'].' already exists.';
+        $summary = 'Failed';
     } elseif ($_GET['action'] == 4) {
         $log = "Fingerprint doesn't match.";
+        $summary = 'Not matched';
     }
 
     $db->update("LatestAudit", "latest", [
-        "log"     => $log
+        "log"     => $summary
     ]);
 
     $db->insert("Audits", [
